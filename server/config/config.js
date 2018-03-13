9efbc148043d8cdc616c8fc8c6cdddc76528c3ac
Daramola Ajiboye <daramolaajibigad@gmail.com>
2018-03-13 12:57:26 +0100

    Add environment variables and updates gitignore

---
+++ server/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE_TEST,
+    host: '127.0.0.1',
+    port: 5432,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.DB_USERNAME,
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'postgres'
+  }
+};
--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
 import dotenv from 'dotenv';
-import dbConfig from './../config/config.json';
+import dbConfig from './../config/config';
 
 dotenv.config();
 const basename = path.basename(module.filename);
