87023c58eefb920e70af45021306712d49a51277
Daramola Ajiboye <daramolaajibigad@gmail.com>
2018-03-20 14:50:58 +0100

    Sets up production database

--- server/config/config.js
+++ server/config/config.js
@@ -18,10 +18,6 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'postgres'
+    use_env_variable: 'DATABASE_URL'
   }
 };
