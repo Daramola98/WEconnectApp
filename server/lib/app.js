c4a036176fc2ce93ee77bb9a08f4c55b91e316b1
Daramola Ajiboye <daramolaajibigad@gmail.com>
2018-03-27 14:24:00 +0100

    Modifies swagger

--- server/lib/app.js
+++ server/lib/app.js
@@ -21,14 +21,13 @@ app.use(validator());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // CORS
-/*
+
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
   next();
 });
-*/
 
 // EXPRESS MIDDLEWARES
 app.use(cors({ credentials: true, origin: true }));
